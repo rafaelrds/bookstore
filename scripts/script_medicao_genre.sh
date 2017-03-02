c5339466f98c57ca8902356ba7588eb299e04684
rafaelrds <crafards@gmail.com>
2017-03-02 03:19:16 -0300

    medicao fixed

--- scripts/script_medicao_genre.sh
+++ scripts/script_medicao_genre.sh
@@ -4,9 +4,9 @@ sleeptime=$((60/$2));
 for i in $(eval echo {1..$2});
 do	
 	if [ "$1" == "GET" ]; then
-		curl -s -w "\nTotal Time: %{time_total}\n" -o NUL "http://localhost:3000/api/genre" -H "Accept-Encoding: gzip, deflate, sdch, br" -H "Accept-Language: en-US,en;q=0.8,pt-BR;q=0.6,pt;q=0.4" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -H "Accept: application/json, text/plain, */*" -H "Referer: http://localhost:3000/" -H "Cookie: io=ZHW35WcjRKQDRam3AAAE" -H "Connection: keep-alive" --compressed
+		curl -s -w "\nTotal Time: %{time_total}\n" -o NUL "http://localhost:3000/api/genres"
 	elif [ "$1" == "POST" ]; then
-		curl -s -w "\nTotal Time: %{time_total}\n" -o NUL "http://localhost:3000/api/genre/" -H "Origin: http://localhost:3000" -H "Accept-Encoding: gzip, deflate, br" -H "Accept-Language: en-US,en;q=0.8,pt-BR;q=0.6,pt;q=0.4" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -H "Content-Type: application/json;charset=UTF-8" -H "Accept: application/json, text/plain, */*" -H "Referer: http://localhost:3000/" -H "Cookie: io=ZHW35WcjRKQDRam3AAAE" -H "Connection: keep-alive" --data-binary "{\"name\":\"adsd\"}" --compressed
+		curl -s -w "\nTotal Time: %{time_total}\n" -o NUL -X POST -H "Content-Type:application/json" "http://localhost:3000/api/genres" -d '{"name":"random genre"}'
 	fi
-	sleep $sleeptime
+	sleep 1
 done
